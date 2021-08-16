7883d584c29967654a800e0cbb0accaec5b93152
JMJM2510 <jjaimesm907@ulacit.ed.cr>
2021-08-16 09:43:29 -0600

    Secured Note related routes

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
 const router = Router();
 const { createNewNote, renderNotes, deleteNote, send} = require('../controllers/note.controller');
+const {isAuthenticated} = require('../helpers/auth');
 
-router.post('/notes/new-note',createNewNote,send);
+router.post('/notes/new-note',isAuthenticated,createNewNote,send);
 
-router.get('/notes',renderNotes);
+router.get('/notes',isAuthenticated,renderNotes);
 
-router.delete('/notes/delete/:id', deleteNote); 
+router.delete('/notes/delete/:id',isAuthenticated, deleteNote); 
 
 module.exports = router;
